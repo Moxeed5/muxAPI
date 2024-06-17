b3afa2c1e24781f4bfea87ded833201fd6d1019e
Max Morgan <40898116+Moxeed5@users.noreply.github.com>
2024-06-16 21:02:33 -0500

    creating api with sql queries

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type Product struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Quantity int `json:"quantity"`
+}
+
+
+func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//router
+	router := mux.NewRouter()
+	//route handlers
+	router.HandleFunc("/product", getProducts(db)).Methods("GET")
+	router.HandleFunc("/product/:id", getProductByID(db)).Methods("GET")
+	router.HandleFunc("/product", addProduct(db)).Methods("POST")
+	router.HandleFunc("/product/:id", updateProduct(db)).Methods(("PUT"))
+	router.HandleFunc("/product/:id", deleteProduct(db)).Methods(("DELETE"))
+
+	//start server
+	log.Fatal(http.ListenAndServe(":8080", jsonContentTypeMiddleware(router)))
+}
+
+//this middleware func is wrapped around the router
+//any incomding request first hits the middleware func, and all it does is set the response type to json and then call the "next func"
+//which is going to be the func that matches the router path. i.e. a get request to /products would first hit middleware and then call getproducts
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w,r)
+	})
+}
+
+//get all products
+func getProducts(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM products")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		products := []Product{}
+
+		for rows.Next() {
+			var p Product 
+			if err := rows.Scan(&p.ID, &p.Name, &p.Quantity); err != nil {
+				log.Fatal(err)
+			}
+
+			products = append(products, p)
+		}
+
+		if err := rows.Err(); err != nil {
+			log.Fatal()
+		}
+
+		json.NewEncoder(w).Encode(products)
+	}
+}
+
+//with mux, Vars returns a map. the keys are param names defined in the route and values are what is extracted from the URL.
+func getProductByID(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter,r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		var p Product
+
+		if err := db.QueryRow("SELECT * FROM products WHERE id = $1", id).Scan(&p.ID,&p.Name,&p.Quantity); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(p)
+	}
+}
+
+//decode json from body of request into p which is a struct instance and then insert into db
+func addProduct(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var p Product 
+		json.NewDecoder(r.Body).Decode(&p )
+
+		if err := db.QueryRow("INSERT INTO products (name, quantity) VALUES ($1, $2) RETURNING id", p.Name, p.Quantity)
+	}
+}
